fecca6ad2cfad6ced74218283d0c7bae9f6a3c15
Maysummer <udojyk@gmail.com>
2023-11-22 15:14:29 +0100

    implement user input

--- src/Register.js
+++ src/Register.js
@@ -66,10 +66,52 @@ const Register = () => {
     setErrMsg('');
   }, [user, pwd, matchPwd]);
 
-  return (<section>
-    <p ref={userRef} className={errMsg ? 'errMsg' : 'offscreen'} aria-live='assertive'>{errMsg}</p>
-    <h1>Register</h1>
-  </section>);
+  return (
+    <section>
+      <p
+        ref={userRef}
+        className={errMsg ? 'errMsg' : 'offscreen'}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Register</h1>
+      <form>
+        <label htmlFor="username">
+          Username:{' '}
+          <span className={validName ? 'valid' : 'hide'}>
+            <FontAwesomeIcon icon={faCheck} />
+          </span>
+          <span className={validName || !user ? 'hide' : 'invalid'}>
+            <FontAwesomeIcon icon={faTimes} />
+          </span>
+        </label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          required
+          aria-invalid={validName ? 'false' : 'true'}
+          aria-describedby="uidnote"
+          onFocus={() => setUserFocus(true)}
+          onBlur={() => setUserFocus(false)}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <p
+          id="uidnote"
+          className={
+            userFocus && user && !validName ? 'instructions' : 'offscreen'
+          }
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          4 to 24 characters. <br />
+          Must begin with a letter. <br />
+          Letters, numbers, underscores, hyphens allowed.
+        </p>
+      </form>
+    </section>
+  );
 };
 
 export default Register;
